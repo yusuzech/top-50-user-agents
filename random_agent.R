8ee619bade3a409c8236dbf39fef97da37e91b4d
Yifu Yan <yanyifu94@hotmail.com>
2018-05-10 18:12:08 -0700

    Update random_agent.R

--- random_agent.R
+++ random_agent.R
@@ -1,5 +1,5 @@
-agent_table <- read.csv("https://raw.githubusercontent.com/yusuzech/top-50-user-agents/master/user_agent.csv")
-agent_list <- agent_table[["User agent"]]
+agent_table <- read.csv("https://raw.githubusercontent.com/yusuzech/top-50-user-agents/master/user_agent.csv",stringsAsFactors = F)
+agent_list <- agent_table[["User.agent"]]
 random_agent <- function(){
-  httr::user_agent(sample(agent_list,1))
-  }
+    httr::user_agent(sample(agent_list,1))
+    }
